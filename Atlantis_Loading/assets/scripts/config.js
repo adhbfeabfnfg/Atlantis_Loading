9620ad7b3b334b974d2a50cff4ed774c6ce634b5
adhbfeabfnfg <150395886+adhbfeabfnfg@users.noreply.github.com>
2023-11-09 23:24:40 +0100

    Add files via upload

---
+++ Atlantis_Loading/assets/scripts/config.js
@@ -0,0 +1,18 @@
+Config = {}; // Don't touch
+
+// Social media buttons on the left side
+Config.Socials = [
+    {name: "discord", icon: "assets/media/icons/discord.png", link: "https://discord.gg/atlantisroleplay"},
+
+];
+
+Config.HideoverlayKeybind = 112 // JS key code https://keycode.info
+Config.CustomBindText = "F1"; // leave as "" if you don't want the bind text in html to be statically set
+
+// Team list
+Config.Team = [
+    {name: "shomii.", description: "Founder", color: "#fff", image: "https://o.remove.bg/downloads/76dd8947-c2d9-485f-8d0e-6cfebce3f6f4/atlantis-removebg-preview.png"},
+    {name: "szzer.", description: "Founder", color: "#fff", image: "https://o.remove.bg/downloads/76dd8947-c2d9-485f-8d0e-6cfebce3f6f4/atlantis-removebg-preview.png"},
+    {name: "vaskezz.", description: "Founder", color: "#fff", image: "https://o.remove.bg/downloads/76dd8947-c2d9-485f-8d0e-6cfebce3f6f4/atlantis-removebg-preview.png"},
+    {name: "GrGa", description: "Developer", color: "#fff", image: "https://o.remove.bg/downloads/76dd8947-c2d9-485f-8d0e-6cfebce3f6f4/atlantis-removebg-preview.png"},
+];
---
+++ Atlantis_Loading/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'highrider#2873'
+description 'CorleoneRP-Loading'
+version '0.1'
+
+files {
+    '*.html',
+    'assets/**/*.*',
+    'assets/**/**/*.*'
+}
+
+
+--loadscreen_manual_shutdown "yes"
+loadscreen 'index.html'
